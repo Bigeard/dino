8a7e2ee1afe4cdf12b574c4c16b2c587bb05f47c
Bigeard <robin.bigeard@gmail.com>
2020-12-18 12:22:33 +0100

    feat(app): add form create user idb and init page game

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,11 +1,23 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VueIdb from "../idb";
 
 Vue.use(Vuex);
+const debug = process.env.NODE_ENV !== "production";
+let modules = VueIdb.modules;
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    hydrated: false
+  },
   actions: {},
-  modules: {}
+  mutations: {
+    DELETE_INDEXED_DB() {}
+  },
+  modules: modules,
+  getters: {
+    hydrated: state => state.hydrated
+  },
+  plugins: [VueIdb.plugin],
+  strict: debug
 });
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -7,46 +7,128 @@
         <gb-input
           label="Search Game !"
           placeholder="Enter the code..."
-          disabled
+          v-model="code_game"
+          :disabled="!user.pass_id"
         />
-        <!-- v-model="synchronizedValue" -->
-        <gb-button disabled right-icon="add">New Game</gb-button>
-        <gb-button disabled right-icon="search">
+        <gb-button
+          :disabled="!user.pass_id"
+          @click="$router.push('/game')"
+          right-icon="add"
+        >
+          New Game
+        </gb-button>
+        <gb-button
+          :disabled="!user.pass_id"
+          @click="$router.push('/game')"
+          right-icon="search"
+        >
           My Games
         </gb-button>
         <gb-button @click="$router.push('/about')" right-icon="info">
           About
         </gb-button>
-
-        <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
       </div>
       <gb-divider />
       <!-- User Info -->
       <div class="user">
         <div class="username">
           <gb-input
+            v-model="username"
             label="Username"
             placeholder="Username is required"
-            info="Please add a username to start playing"
+            :info="info"
+            :error="error"
+            :status="status"
           />
-          <gb-button>✔️</gb-button>
+          <gb-button @click="checkUsername" :class="status + '_valid'"
+            >✔️</gb-button
+          >
         </div>
-        <!-- error="A minimum of 3 characters is required" -->
-        <p>Pass ID: XGFi3SHgXGFi3SHgXGFi3SHg</p>
+        <p v-if="user.pass_id">
+          Pass ID :
+          <span class="pass_id">{{ user.pass_id }}</span>
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home"
-  // ,components: {
-  //   // HelloWorld
-  // }
+  name: "Home",
+  async beforeMount() {
+    let user = await this.$db.user.get({ id: 0 });
+    if (user === undefined) {
+      user = {
+        id: 0,
+        username: "",
+        pass_id: null,
+        updated_at: new Date()
+      };
+      await this.$db.user.add(user);
+    }
+    this.onChangeUsername(user.username);
+    if (this.status === "normal") {
+      this.username = user.username;
+      this.user = user;
+    }
+  },
+  data() {
+    return {
+      code_game: null,
+      username: null,
+      info: null,
+      error: null,
+      status: "normal",
+      user: {
+        username: null,
+        pass_id: null
+      }
+    };
+  },
+  watch: {
+    code_game(v) {
+      if (v.length > 8) {
+        this.$router.push("/game");
+      }
+    },
+    username(v) {
+      this.onChangeUsername(v);
+    }
+  },
+  methods: {
+    onChangeUsername(v) {
+      if (v.length === 0) {
+        this.info = "Please add a username to start playing";
+        this.status = "warning";
+        this.error = null;
+      } else if (v.length < 3) {
+        this.error = "A minimum of 3 characters is required";
+        this.status = "error";
+        this.info = null;
+      } else if (!/^[\w.]*$/.test(v)) {
+        this.error =
+          'you can only use the following characters: "A-z" "0-9" "_"';
+        this.status = "error";
+        this.info = null;
+      } else {
+        this.info = null;
+        this.error = null;
+        this.status = "normal";
+      }
+    },
+    async checkUsername() {
+      if (this.status === "normal") {
+        const user = {
+          id: 0,
+          username: this.username,
+          pass_id: "EUYahAs3u77YP9Bb"
+        };
+        await this.$db.user.update(0, user);
+        this.user = user;
+      }
+    }
+  }
 };
 </script>
 
@@ -55,6 +137,12 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 100vh;
+
+  .error_valid,
+  .warning_valid {
+    display: none;
+  }
 
   .content {
     border: 1px solid #3f536e;
@@ -62,6 +150,7 @@ export default {
     background-color: #171e29;
     padding: 30px;
     margin-top: 30px;
+    margin: 6px 6px 30px 6px;
   }
 
   .logo {
@@ -72,7 +161,8 @@ export default {
 
   .username {
     display: flex;
-    align-items: center;
+    align-items: flex-end;
+    margin-bottom: 30px;
     .gb-field-input {
       width: 100%;
       margin-right: 10px;
@@ -80,6 +170,14 @@ export default {
     .gb-base-button {
       height: 42px;
     }
+  }
+
+  .pass_id {
+    margin-left: 10px;
+    background-color: #222c3c;
+    box-shadow: 0 1px 5px 0 #18191a;
+    padding: 10px;
+    border-radius: 4px;
   }
 
   .nav {
