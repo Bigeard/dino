c2f9ee24f211c09d33d276c0dd763b68f1a7a0fd
CorentinGlrd5 <corentin.guillard@ynov.com>
2021-01-08 14:45:01 +0100

    feat(app): start room

--- app/src/idb/index.js
+++ app/src/idb/index.js
@@ -6,12 +6,24 @@ Vue.use(VueIdb);
 export default new VueIdb({
   version: 1,
   database: "dino",
-  schemas: [{ user: "id, username, pass_id, updated_at" }],
+  schemas: [
+    { user: "_id, username, pass_id, updated_at" },
+    {
+      game:
+        "_id, name, map, width, height, players, closeDialogWin, status, created_at, updated_at"
+    }
+  ],
   options: {
     user: {
       type: "list",
-      primary: "id",
+      primary: "_id",
       label: "username",
+      updated_at: "updated_at"
+    },
+    game: {
+      type: "list",
+      primary: "_id",
+      label: "name",
       updated_at: "updated_at"
     }
   }
