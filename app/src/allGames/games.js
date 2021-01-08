e6c6e0a6ca9f519ae7edccba38e18636e751994a
clem-pgx <yeticlem79@hotmail.fr>
2021-01-08 11:57:17 +0100

    add fake data

---
+++ app/src/allGames/games.js
@@ -0,0 +1,5 @@
+export const games = [
+    { id: 1, code: 12345, created_at: '01/01/2021', update_at: '08/01/2021', status: true },
+    { id: 2, code: 123, created_at: '02/01/2021', update_at: '07/01/2021', status: false },
+    { id: 3, code: 1235, created_at: '04/01/2021', update_at: '05/01/2021', status: false }
+]
