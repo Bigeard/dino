5bd6f98ae5fc4c2bd8472d12ddf48b78b024be66
clem-pgx <yeticlem79@hotmail.fr>
2021-01-12 21:55:57 +0100

    UPD bg-color card

--- app/src/allGames/games.js
+++ app/src/allGames/games.js
@@ -1,5 +1,26 @@
 export const games = [
-    { id: 1, code: 12345, created_at: '01/01/2021', update_at: '08/01/2021', status: true },
-    { id: 2, code: 123, created_at: '02/01/2021', update_at: '07/01/2021', status: false },
-    { id: 3, code: 1235, created_at: '04/01/2021', update_at: '05/01/2021', status: false }
-]
+  {
+    id: 1,
+    code: 12345,
+    created_at: "01/01/2021",
+    update_at: "08/01/2021",
+    status: true,
+    name: "Game 1"
+  },
+  {
+    id: 2,
+    code: 123,
+    created_at: "02/01/2021",
+    update_at: "07/01/2021",
+    status: false,
+    name: "Game 2"
+  },
+  {
+    id: 3,
+    code: 1235,
+    created_at: "04/01/2021",
+    update_at: "05/01/2021",
+    status: false,
+    name: "Game 3"
+  }
+];
