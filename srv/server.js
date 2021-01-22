4dec42d995f8539d52d313e185cc8377434c6ad5
Axel-Ollivier <axel.ollivier56@outlook.com>
2021-01-22 10:37:41 +0100

    feat(app,srv): add user create and read requests

--- srv/server.js
+++ srv/server.js
@@ -6,6 +6,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var corsOptions = {
   origin: ["http://localhost:${PORT}"]
 };
